1b819b1a0a6a1f8f47d9d4fcb14d849e7973d55a
Javier Calatrava <javiercalatrava@capitole-consulting.com>
2021-10-03 07:59:52 +0200

    Created empty blank project

---
+++ EnvironmentObject/EnvironmentObject/EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectApp.swift
+//  EnvironmentObject
+//
+//  Created by Javier Calatrava on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
